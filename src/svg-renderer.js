d5b200e952119785ab35f4e2eadd847454268384
Michael "Z" Goddard <mzgoddard@gmail.com>
2019-01-30 17:16:28 -0500

    measure a cloned svg node

--- src/svg-renderer.js
+++ src/svg-renderer.js
@@ -361,32 +361,25 @@ class SvgRenderer {
      * a natural and performant way.
      */
     _transformMeasurements () {
-        // Save `svgText` for later re-parsing.
-        const svgText = this.toString();
-
         // Append the SVG dom to the document.
         // This allows us to use `getBBox` on the page,
         // which returns the full bounding-box of all drawn SVG
         // elements, similar to how Scratch 2.0 did measurement.
         const svgSpot = createSvgSpot();
+        // Clone the svg tag. This tag becomes unusable/undrawable in browsers
+        // once it's appended to the page, perhaps for security reasons?
+        let tempTag = this._svgTag.cloneNode(/* deep */ true);
         let bbox;
         try {
-            svgSpot.appendChild(this._svgTag);
+            svgSpot.appendChild(tempTag);
             document.body.appendChild(svgSpot);
             // Take the bounding box.
-            bbox = this._svgTag.getBBox();
+            bbox = tempTag.getBBox();
         } finally {
             // Always destroy the element, even if, for example, getBBox throws.
             document.body.removeChild(svgSpot);
-            svgSpot.removeChild(this._svgTag);
+            svgSpot.removeChild(tempTag);
         }
-
-        // Re-parse the SVG from `svgText`. The above DOM becomes
-        // unusable/undrawable in browsers once it's appended to the page,
-        // perhaps for security reasons?
-        const parser = new DOMParser();
-        this._svgDom = parser.parseFromString(svgText, 'text/xml');
-        this._svgTag = this._svgDom.documentElement;
 
         // Enlarge the bbox from the largest found stroke width
         // This may have false-positives, but at least the bbox will always
